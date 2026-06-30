971548cc5a4369506fc2b54b7348dd0eb31c24cc
vikash <vikashsuman886@gmail.com>
2026-06-30 19:28:58 +0530

    remove prod environment keep only dev

--- terraform/backend/prod.hcl
+++
@@ -1,5 +0,0 @@
-bucket         = "CHANGE-ME-prod-terraform-state-bucket"
-key            = "eks/prod/terraform.tfstate"
-region         = "ap-south-1"
-dynamodb_table = "CHANGE-ME-terraform-locks"
-encrypt        = true
--- terraform/envs/prod/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "cluster_name" { value = module.eks.cluster_name }
-output "cluster_endpoint" { value = module.eks.cluster_endpoint }
-output "vpc_id" { value = module.vpc.vpc_id }
-output "ecr_repository_url" { value = module.ecr.repository_url }
-output "alb_dns_name" { value = module.alb.alb_dns_name }
-output "jenkins_public_ip" { value = module.jenkins.public_ip }
